b0666ddda6a44a20ba5126038207f8395107ab93
Darkknight123 <philipkerry57@gmail.com>
2022-01-27 10:35:05 +0300

    change the number of sides on your dice

--- src/First.kt
+++ src/First.kt
@@ -4,13 +4,16 @@ fun main(){
     val diceRoll = myFirstDice.roll()
 
     println("Your ${myFirstDice.sides} sided dice rolled ${diceRoll}")
+
+    myFirstDice.sides = 20
+    println("Your ${myFirstDice.sides} sided dice rolled ${diceRoll}")
 }
 
 class First {
     var sides = 6
 
     fun roll(): Int{
-        val randomNumber = (1..6).random()
+        val randomNumber = (1..sides).random()
         return randomNumber
     }
 }
