a4b18e53c684afa72c5e1b61374f783291725499
Darkknight123 <philipkerry57@gmail.com>
2022-02-04 14:28:55 +0300

    a roundHut subclass

--- src/First.kt
+++ src/First.kt
@@ -15,6 +15,32 @@ fun main(){
         override val capacity = 6
 
     }
+
+    //instance of SquareCabin
+    val squareCabin = SquareCabin(6)
+
+    println("\nSquare Cabin\n======")
+    println("Capacity:${squareCabin.capacity}")
+    println("Material: ${squareCabin.buildingMaterial}")
+    println("Has room? ${squareCabin.hasRoom()}")
+
+
+    //A subclass of Dwelling it inherits from it
+    class RoundHut(residents:Int):Dwelling(residents){
+
+        override val buildingMaterial = "Straw"
+        override val capacity = 4
+    }
+
+    //instance of RoundHut
+    val roundHut = RoundHut(3)
+
+    with(roundHut){
+        println("\nRoundHut\n=======")
+        println("Capacity:${capacity}")
+        println("Material:${buildingMaterial}")
+        println("Has Room?: ${hasRoom()}")
+    }
 }
 
 
